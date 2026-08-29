44f9ad941b3181ab3eaa39243635d7da09b65cf7
vimanoelkkj <vitormanoelalmeida2024@gmail.com>
2026-08-29 03:24:44 -0300

    refactor: expose shared focusable selector

--- public/assets/js/utils/focus.js
+++ public/assets/js/utils/focus.js
@@ -1,10 +1,8 @@
-const FOCUSABLE =
+export const FOCUSABLE_SELECTOR =
   'button:not([disabled]), a[href], input:not([disabled]), textarea:not([disabled]), select:not([disabled]), [tabindex]:not([tabindex="-1"])';
-
 export function focusFirst(container) {
-  container?.querySelector(FOCUSABLE)?.focus();
+  container?.querySelector(FOCUSABLE_SELECTOR)?.focus();
 }
-
 export function restoreFocus(element) {
   if (element instanceof HTMLElement && element.isConnected) element.focus();
 }
