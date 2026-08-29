280638c00aaf5ad5da10abaa41d0cc3c1a11954c
vimanoelkkj <vitormanoelalmeida2024@gmail.com>
2026-08-29 03:05:15 -0300

    feat: add aria state helpers

---
+++ public/assets/js/utils/aria.js
@@ -0,0 +1,9 @@
+export function ariaExpanded(value) {
+  return value ? "true" : "false";
+}
+export function ariaHidden(value) {
+  return value ? "true" : "false";
+}
+export function ariaDisabled(value) {
+  return value ? "true" : "false";
+}
