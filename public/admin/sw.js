6db5ac40059024724544cdb0a8c3cb46490970fd
v1t0rrr <pondebatata@outlook.com>
2026-08-30 20:36:56 -0300

    style: format admin service worker

--- public/admin/sw.js
+++ public/admin/sw.js
@@ -26,7 +26,13 @@ self.addEventListener("activate", event => {
   event.waitUntil(
     caches
       .keys()
-      .then(keys => Promise.all(keys.filter(key => key.startsWith("rp-admin-shell-") && key !== CACHE).map(key => caches.delete(key))))
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(key => key.startsWith("rp-admin-shell-") && key !== CACHE)
+            .map(key => caches.delete(key))
+        )
+      )
       .then(() => self.clients.claim())
   );
 });
