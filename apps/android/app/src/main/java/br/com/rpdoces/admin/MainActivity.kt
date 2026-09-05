506d3bd0a939fb17b3e1fe0abdd5cc10650188b8
vimanoelkkj <vitormanoelalmeida2024@gmail.com>
2026-09-05 02:47:42 -0300

    feat(android): ativa gate biométrico nativo

--- apps/android/app/src/main/java/br/com/rpdoces/admin/MainActivity.kt
+++ apps/android/app/src/main/java/br/com/rpdoces/admin/MainActivity.kt
@@ -1,14 +1,14 @@
 package br.com.rpdoces.admin
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import br.com.rpdoces.admin.ui.RPApp
+import androidx.fragment.app.FragmentActivity
+import br.com.rpdoces.admin.ui.BiometricRPApp
 import br.com.rpdoces.admin.ui.theme.RPTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge(
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
         val container = (application as RPApplication).container
         setContent {
             RPTheme {
-                RPApp(
+                BiometricRPApp(
                     authRepository = container.authRepository,
                     dashboardRepository = container.dashboardRepository,
                     productsRepository = container.productsRepository,
