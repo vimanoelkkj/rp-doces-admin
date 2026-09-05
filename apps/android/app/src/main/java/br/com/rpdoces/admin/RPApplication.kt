8d84b788980cb363fb6e83cecf7aac2e894ca14e
vimanoelkkj <vitormanoelalmeida2024@gmail.com>
2026-09-05 02:23:28 -0300

    feat(android): registra repositorios das demais telas

--- apps/android/app/src/main/java/br/com/rpdoces/admin/RPApplication.kt
+++ apps/android/app/src/main/java/br/com/rpdoces/admin/RPApplication.kt
@@ -1,10 +1,14 @@
 package br.com.rpdoces.admin
 
 import android.app.Application
+import br.com.rpdoces.admin.data.admins.AdminsRepository
 import br.com.rpdoces.admin.data.auth.AuthRepository
 import br.com.rpdoces.admin.data.dashboard.DashboardRepository
 import br.com.rpdoces.admin.data.network.ApiClient
 import br.com.rpdoces.admin.data.network.SessionCookieJar
+import br.com.rpdoces.admin.data.orders.OrdersRepository
+import br.com.rpdoces.admin.data.products.ProductsRepository
+import br.com.rpdoces.admin.data.store.StoreRepository
 
 class RPApplication : Application() {
     lateinit var container: AppContainer
@@ -27,4 +31,8 @@ class AppContainer(application: Application) {
     )
 
     val dashboardRepository = DashboardRepository(apiClient.retrofit)
+    val productsRepository = ProductsRepository(apiClient.retrofit)
+    val ordersRepository = OrdersRepository(apiClient.retrofit)
+    val adminsRepository = AdminsRepository(apiClient.retrofit)
+    val storeRepository = StoreRepository(apiClient.retrofit)
 }
