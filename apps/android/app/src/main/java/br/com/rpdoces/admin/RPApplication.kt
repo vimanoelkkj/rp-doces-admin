05e6a04727c3ad87e5ad91b5b90241397520b7d1
vimanoelkkj <vitormanoelalmeida2024@gmail.com>
2026-09-05 00:07:07 -0300

    feat(android): adiciona container de dependencias

---
+++ apps/android/app/src/main/java/br/com/rpdoces/admin/RPApplication.kt
@@ -0,0 +1,27 @@
+package br.com.rpdoces.admin
+
+import android.app.Application
+import br.com.rpdoces.admin.data.auth.AuthRepository
+import br.com.rpdoces.admin.data.network.ApiClient
+import br.com.rpdoces.admin.data.network.SessionCookieJar
+
+class RPApplication : Application() {
+    lateinit var container: AppContainer
+        private set
+
+    override fun onCreate() {
+        super.onCreate()
+        container = AppContainer(this)
+    }
+}
+
+class AppContainer(application: Application) {
+    private val sessionCookieJar = SessionCookieJar(application)
+    private val apiClient = ApiClient(sessionCookieJar)
+
+    val authRepository = AuthRepository(
+        retrofit = apiClient.retrofit,
+        json = apiClient.json,
+        cookieJar = sessionCookieJar
+    )
+}
